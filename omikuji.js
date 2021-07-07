4ae597df7a5f5114c8f245b3a4d545888e1a429f
Kunionick <carpediem.sbr7@gmail.com>
2021-07-07 20:37:57 +0900

    second commit

--- omikuji.js
+++ omikuji.js
@@ -5,4 +5,9 @@ let kujis = ['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶'];
 let kuji = Math.trunc(Math.random() * kujis.length);
 //console.log(kuji);
 
-document.write('<p>あなたの今日の運勢は' + kujis[kuji] + 'です！<p>');
+// document.write('<p>あなたの今日の運勢は' + kujis[kuji] + 'です！<p>');
+
+let buttonClick = () => {
+  // target = document.getElementById('text').innerHTML = '<p>あなたの今日の運勢は' + kujis[kuji] + 'です！<p>';
+  target = document.getElementById('text').textContent = 'あなたの今日の運勢は' + kujis[kuji] + 'です！';
+};
